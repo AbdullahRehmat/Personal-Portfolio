9b894fd51836bbf06915f722aa2b6282838b390e
AbdullahRehmat <rehmat.dev@gmail.com>
2021-12-29 18:43:19 +0300

    Website is now responsive

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ def static_from_root():
 
 
 if __name__ == "__main__":
-    #app.run(debug=True, host="0.0.0.0", port=5000)
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=5000)
+    #app.run()
